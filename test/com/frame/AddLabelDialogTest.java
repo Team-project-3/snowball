041f66c411393e5a4917367f125d8bb6af48e1e8
LiaoZijun <937558613@qq.com>
2021-05-19 13:26:35 +0800

    finish GUI test of AddLabelDialog

--- test/com/frame/AddLabelDialogTest.java
+++ test/com/frame/AddLabelDialogTest.java
@@ -1,41 +1,58 @@
 package com.frame;
 
-import java.awt.Frame;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JDialogOperator;
+import org.netbeans.jemmy.operators.JTextAreaOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+
 import com.data.Label;
 
 class AddLabelDialogTest {
+	AddLabelDialog dialog;
+    JDialogOperator dlgOpr;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		dialog = new AddLabelDialog();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		
 	}
 
 	@Test
 	void testShow() {
-		AddLabelDialog dialog = new AddLabelDialog();
-		Frame frame = new Frame();
-		
-		dialog.show(frame);
+		dialog.show(null, true);
 	}
 
 	@Test
-	void testGetLabel() {
-		AddLabelDialog dialog = new AddLabelDialog();
-		Frame frame = new Frame();
+	void testGetLabel() throws InterruptedException {
+		dialog.show(null, false);
 		
-		dialog.show(frame);
+		dlgOpr = new JDialogOperator("添加标签");
+		JTextFieldOperator labelOpr = new JTextFieldOperator(dlgOpr, 0);
+		
+		Assert.assertEquals("", labelOpr.getText());
+		labelOpr.setText("是否与股票相关");
+		Assert.assertEquals("是否与股票相关", labelOpr.getText());
+		
+		JTextAreaOperator optionOpr = new JTextAreaOperator(dlgOpr, 0);
+		Assert.assertEquals("", optionOpr.getText());
+		optionOpr.append("是\n否");
+		Assert.assertEquals("是\n否", optionOpr.getText());
+		
+		JButtonOperator btnOpr = new JButtonOperator(dlgOpr, "添加");
+		btnOpr.doClick();
 		
 		Label labeltest = dialog.getLabel();
-		Assert.assertEquals("测试", labeltest.getContent());
-		Assert.assertEquals("hello", labeltest.getOptions().get(0));
-		Assert.assertEquals("world", labeltest.getOptions().get(1));
+		Assert.assertEquals("是否与股票相关", labeltest.getContent());
+		Assert.assertEquals("是", labeltest.getOptions().get(0));
+		Assert.assertEquals("否", labeltest.getOptions().get(1));
 	}
 
 }
